ac9829977526ee30bf4024175d25937cb233cb59
Anass Taher <AnassTaher2003@hotmail.com>
2023-08-10 17:01:38 +0200

    init commit, not using full vite anymore

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      bg : "#d2c1b0",
+      fg : "#37251b",
+      compl : "#8f755f",
+      op1 : "#ad6d2f",
+      op2 : "#e4bc84",
+      op3 : "#80411e",
+    }
+  },
+  plugins: [],
+}
